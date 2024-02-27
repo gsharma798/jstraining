e2ad386c6a667c9dc7785b9d815214103e5a74e6
= <=>
2024-02-27 20:46:45 +0530

    New Updations

--- function_practice.js
+++ function_practice.js
@@ -42,24 +42,82 @@
 
 // ###### Q2. Write a JavaScript function that checks whether a passed string is a palindrome or not? ######
 
-function palindrome(input_value)
-{
-    let pali = input_value.split("").reverse().join();
-    pali = pali.split();
-    console.log(pali);
-    console.log(typeof pali);
-    input_value = input_value.split("");
-    console.log(input_value);
-    console.log(typeof input_value);
-    if(pali == input_value)
-    {
-        console.log("Input recieved is palindrome ", input_value);
-    }
-    else
-    {
-        console.log("Input recieved is not palindrome ", input_value);
-    }
+// function palindrome(input_value)
+// {
+//     let pali = input_value.split("").reverse().join();
+//     pali = pali.split();
+//     console.log(pali);
+//     console.log(typeof pali);
+//     input_value = input_value.split("");
+//     console.log(input_value);
+//     console.log(typeof input_value);
+//     if(pali == input_value)
+//     {
+//         console.log("Input recieved is palindrome ", input_value);
+//     }
+//     else
+//     {
+//         console.log("Input recieved is not palindrome ", input_value);
+//     }
     
+// }
+// let result = palindrome("madam");
+// console.log(result);
+
+
+// ###### Q3. Write a function that returns the square of a number ######
+
+
+// function findSquare(num)
+// {
+//     return num ** 2;
+// }
+
+// var result = findSquare(12);
+// console.log(result)
+
+
+
+// ###### Q4. Write a function to find the area of a given Rectangle ######
+
+// function fin_rect_area(length, width)
+// {
+//     return length * width;
+// }
+
+// var result = fin_rect_area(98,98,);
+// console.log(result);
+
+
+// ###### Q5. Write a function to find the area and perimeter of a Circle ######
+
+// function circle_area(radius)
+// {
+//     const pie = 3.14;
+//     return pie * (radius ** 2);
+// }
+
+// var result = circle_area(20);
+// console.log(result);
+
+
+// ###### Q6. Write a function to reverse a number ######
+
+// function reverseNum(num)
+// {
+//     return num.split("").reverse().join();
+// }
+
+// var result = reverseNum("12345.sff");
+// console.log(result);
+
+
+// ###### Q6. Write a function to to use rest operator ######
+
+function Rest_opt(...rest)
+{
+    return rest * rest;
 }
-let result = palindrome("madam");
-console.log(result);
+
+var res = Rest_opt(23, 23, 32, 89, 89, 89);
+console.log(res);
